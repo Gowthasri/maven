a27f0d052dc3c309b94c88c64d1aa550c279217b
Gowthasri Nallamilli <gowthasri.reddynallamilli@contractor.ca.com>
2018-03-07 08:57:31 -0600

    master added new function

---
+++ loops/src/whileloop.java
@@ -0,0 +1,12 @@
+public class whileloop{
+    public int isEvenumber(int x){
+        while(x%2==0){
+            System.out.println(x + " is an even number");
+        }
+        System.out.println(x + "is not an even number");
+
+    public static void main(String[] args) {
+   isEvenumber(5);
+
+}
+}
